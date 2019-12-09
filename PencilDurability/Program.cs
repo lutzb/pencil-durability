7c437d85d9dd2858d02e3fd14813ba9ff0a83216
BryanLutz <Bryan.Lutz@united.com>
2019-12-09 13:05:12 -0500

    Initial commit

---
+++ PencilDurability/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PencilDurability
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
