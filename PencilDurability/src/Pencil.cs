473489ed8bae5f56e35d135eea07417aec5ec45e
BryanLutz <Bryan.Lutz@united.com>
2020-01-02 12:51:04 -0500

    Pencil now only writes letters while point is not dull

--- PencilDurability/src/Pencil.cs
+++ PencilDurability/src/Pencil.cs
@@ -1,10 +1,32 @@
 namespace PencilDurability
 {
+    using System.Linq;
+
     public class Pencil
     {
+        private int point;
+
+        public Pencil(int pointDurability)
+        {
+            point = pointDurability;
+        }
+
         public void Write(ref string paper, string textToWrite)
         {
-            paper += textToWrite;
+            var currentPaper = paper;
+            textToWrite.ToList().ForEach(c =>
+            {
+                if (point > 0)
+                {
+                    currentPaper += c;
+                    point--;
+                }
+                else
+                {
+                    currentPaper += " ";
+                }
+            });
+            paper = currentPaper;
         }
     }
 }
--- PencilDurability/test/PencilTest.cs
+++ PencilDurability/test/PencilTest.cs
@@ -8,12 +8,23 @@ namespace PencilDurability.test
         [Test]
         public void Write_WhenGivenPaper_AppendsTextToTheEndOfThePaper()
         {
-            var subject = new Pencil();
+            var subject = new Pencil(50);
             var paper = "She sells sea shells";
 
             subject.Write(ref paper, " down by the sea shore.");
 
             Assert.AreEqual("She sells sea shells down by the sea shore.", paper);
+        }
+
+        [Test]
+        public void Write_OnlyWritesLettersWhilePointIsNotDull()
+        {
+            var subject = new Pencil(5);
+            var paper = "";
+
+            subject.Write(ref paper, "12345678");
+
+            Assert.AreEqual("12345   ", paper);
         }
     }
 }
